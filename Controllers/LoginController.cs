89ca775f4102a1e8c81378472bd2022277d5607c
Terence Nathan <119557391+Terence172@users.noreply.github.com>
2023-05-02 00:34:03 +0800

    Completed Basics

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,67 @@
+﻿using Assignment_3_ASP.NET_MVC_CRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment_3_ASP.NET_MVC_CRUD.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        db_cbsEntities db = new db_cbsEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+
+        public ActionResult Index(tb_user objchk)
+        {
+            
+            if (ModelState.IsValid) 
+            {
+                using(db_cbsEntities db = new db_cbsEntities())
+                {
+
+                    var obj = db.tb_user.Where(a => a.u_name.Equals(objchk.u_name) && a.u_pass.Equals(objchk.u_pass)).FirstOrDefault();
+
+
+                    if (obj != null)
+                    {
+                        Session["UserIC"] = obj.u_ic.ToString();
+                        Session["UserName"] = obj.u_name.ToString();
+                        Session["UserPhoneNo"] = obj.u_phone.ToString();
+                        Session["UserAddress"] = obj.u_address.ToString();
+                
+                        //Return once done :P
+                        return RedirectToAction("Index", "Home");
+                    }
+
+                    else
+                    {
+                        //In Case it is wrong
+                        ModelState.AddModelError("", "The Name or Password is Incorrect");
+                    }
+                }
+            }
+
+
+
+            return View();
+        }
+
+        //Logout Function
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Login");
+        }
+
+    }
+}
